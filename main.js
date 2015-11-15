711cbe11a5f4fd8791995d032a3aed41c3dd9eeb
Foo <node@Alexs-MacBook-Pro.local>
2015-11-15 00:26:59 -0700

    added full text search features for both twitter and govtrack

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function getTweets(){
 // https://github.com/jublonet/codebird-js
+  var twitterSearch = document.getElementById('twitterBox').value;
   var cb = new Codebird;
   cb.setBearerToken("AAAAAAAAAAAAAAAAAAAAAK97igAAAAAAL6M3uxb0OEWglYZcJpxq89e46zY%3Dt682yccM1IgilC04xfWysYugpZ2ZzmaLpIcvNTB9L6xhdjaAXC");
   // I created this CORS proxy using the codebird cors proxy source https://github.com/jublonet/codebird-cors-proxy/
@@ -10,9 +11,9 @@ function getTweets(){
   // var twitterName = document.getElementsById('twitter-seacrh').val();
   cb.__call(
       "statuses_userTimeline",
-      "screen_name=drake",
+      "screen_name="+twitterSearch,
       function (data, rate, err) {
-          console.log(data);
+          // console.log(data);
           // var $result = $('#results');
           for(i = 0; i < data.length; i = i + 1){
             var tweet = data[i];
@@ -20,49 +21,60 @@ function getTweets(){
             $('.tweetz').append(
               "<li class='tweet'>"+ writing +"</li>"
             )
-            console.log(writing)
+            // console.log(writing)
           }
       },
       true // this parameter required
   );
 }
 // getTweets();
-
 function factCheck(){
-  var theURL = 'https://www.govtrack.us/api/v2/bill?format=jsonp';
+  var query = document.getElementById('searchBox').value;
+  var theURL = "https://www.govtrack.us/api/v2/person/?q=" + query;
   $.ajax({
-       dataType: "jsonp",
+       dataType: "json",
        url: theURL,
        cache: true,
        success: function(data){
-       console.log('Success!');
-      //  console.log(data);
+       console.log(data);
        var individualStats = [];
        var titles = [];
        for(var i=0 in data.objects){
-         var title = data.objects[i].title;
-         var first = data.objects[i].sponsor.firstname;
-         var last = data.objects[i].sponsor.lastname;
-         var party = data.objects[i].sponsor_role.party;
-         var twitter = data.objects[i].sponsor.twitterid;
-         individualStats.push(title, first, last, party, twitter);
-         console.log(individualStats);
+         var sort = data.objects[i].sortname;
+         var bioGuide = data.objects[i].bioguideid;
+         var birthday = data.objects[i].birthday;
+         var twitter = data.objects[i].twitterid;
+         var youtube = data.objects[i].youtubeid;
+         var link = data.objects[i].link;
+         //create a person array
+         // create a person object!
+         individualStats.push(sort, bioGuide, birthday, twitter, youtube);
+          // console.log(individualStats);
+          //  var j ;
+          //  for(var j=0; j<individualStats.length;j=j+1){
+          //    console.log('whats good');
+          //  }
            if(twitter != undefined ){
              $('.results').append(
-              "<div class='flexer'><h3>" + title + "<h3>" +
-              "<h4>" + first + ' ' + last + "</h4>" +
-              "<h5>" + party + "</h5>" +
-              "<h6>" + twitter + "</h6></div>"
+              "<div class='flexer'><h3>" + sort + "<h3>" +
+              "<h4>" +  bioGuide + "</h4>" +
+              "<h5>" + birthday + "</h5>" +
+              "<h6>" + twitter + "<br>" + youtube + "<br>" + link + "</h6></div>"
              )
            } else {
              $('.results').append(
-              "<div class='flexer'><h4>" + title + "<h4>" +
-              "<h5>" + first + ' ' + last + "</h5>" +
-              "<h6>" + party + "</h6>"
+               "<div class='flexer'><h3>" + sort + "<h3>" +
+               "<h4>" +  bioGuide + "</h4>" +
+               "<h5>" + birthday + "</h5>" +
+               "<h6>" + twitter + "<br>" + youtube + "<br>" + link +"</h6></div>"
              )
            }
          }
+
        }
+     }).done(function(){
+       console.log('done 1')
+       // could pass getTweets() here, too
      });
    }
   //  factCheck();
